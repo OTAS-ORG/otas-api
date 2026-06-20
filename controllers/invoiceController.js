5c32d4271b12698d8cf43d7a5d58d983ca423d2e
acehidan <aungaungoogame2@gmail.com>
2026-06-20 13:58:17 +0630

    fix: vouncher format dd-mm to mm-yy

--- controllers/invoiceController.js
+++ controllers/invoiceController.js
@@ -5,9 +5,9 @@ const sendResponse = require('../utils/response');
 
 const generateInvoiceNumber = async (type, serviceType) => {
   const now = new Date();
-  const dd = String(now.getDate()).padStart(2, '0');
   const mm = String(now.getMonth() + 1).padStart(2, '0');
-  const datePart = `${dd}${mm}`;
+  const yy = String(now.getFullYear()).slice(-2);
+  const datePart = `${mm}${yy}`;
 
   let prefix = '';
   let query = {};
